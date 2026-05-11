0487756ba7ec1092880ee69d819f70e8dba0cb9f
rlitvinov <romalrni@gmail.com>
2026-05-11 23:46:00 +0300

    Changed multiple app instances prevention mechanism from named semaphore to lockfile

--- IPCTestCommon/include/instance_locker.h
+++ IPCTestCommon/include/instance_locker.h
@@ -1,33 +1,36 @@
 #pragma once
 
-#include <fcntl.h>
-#include <semaphore.h>
 #include <string>
+#include <sys/file.h>
+#include <unistd.h>
 
 class CInstanceLocker
 {
+    const std::string LockFilePath = "/tmp/";
+
 public:
     CInstanceLocker(std::string instanceIdentifier)
-        : m_instanceIdentifier(instanceIdentifier)
+        : m_lockFileName(LockFilePath + instanceIdentifier + ".lock")
     {
-        m_pInstanceSemaphore = sem_open(instanceIdentifier.c_str(), O_CREAT, 0666, 1);
-        m_instanceSemaphoreLocked = (sem_trywait(m_pInstanceSemaphore) != -1);
+        m_lockFileFD = open(m_lockFileName.c_str(), O_CREAT | O_RDWR, 0666);
+        m_instanceLocked = (flock(m_lockFileFD, LOCK_EX | LOCK_NB) == 0) || (errno != EWOULDBLOCK);
     }
 
     ~CInstanceLocker()
     {
-        if (m_instanceSemaphoreLocked)
+        if (m_instanceLocked && (m_lockFileFD != -1))
         {
-            sem_post(m_pInstanceSemaphore);
-            sem_unlink(m_instanceIdentifier.c_str());
+            close(m_lockFileFD);
+            m_lockFileFD = -1;
+            m_instanceLocked = false;
+            remove(m_lockFileName.c_str());
         }
-        sem_close(m_pInstanceSemaphore);
     }
 
-    bool isLocked() const { return m_instanceSemaphoreLocked; }
+    bool isLocked() const { return m_instanceLocked; }
 
 private:
-    sem_t* m_pInstanceSemaphore = nullptr;
-    bool m_instanceSemaphoreLocked = false;
-    std::string m_instanceIdentifier;
+    bool m_instanceLocked = false;
+    std::string m_lockFileName;
+    int m_lockFileFD = -1;
 };
